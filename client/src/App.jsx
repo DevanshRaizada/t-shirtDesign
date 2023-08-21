094d025ae1f60410502e2ddcb34d0552aefab39e
DevanshRaizada <76863326+DevanshRaizada@users.noreply.github.com>
2023-08-21 12:06:19 +0530

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,18 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
+function App() {
+  <div>
+    <h1 className="head-text">Rect App</h1>
+  </div>
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
